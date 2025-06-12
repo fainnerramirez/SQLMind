6139889a0acaa46df417de543a5628d076872834
fainnerramirez <fainnerramirez@gmail.com>
2025-06-12 16:16:18 -0500

    feat: restaurando firebase

--- src/main.tsx
+++ src/main.tsx
@@ -2,26 +2,30 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import App from './App.tsx';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import RedirectAuth from './components/redirect-auth.tsx';
+import HomePage from './pages/home.page.tsx';
+import ProtectedRoutesApp from './components/protected-routes.tsx';
+import AuthInitializer from './components/auth-initializer.tsx';
 
-// const router = createBrowserRouter(
-//   createRoutesFromElements(
-//     <>
-//       <Route path='/' element={<RedirectAuth />}></Route>
-//       <Route path='/login' element={<HomePage />}></Route>
-//       <Route path='/' element={<ProtectedRoutesApp />}>
-//         <Route path='app' element={<App />}></Route>
-//       </Route>
-//     </>
-//   )
-// );
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path='/' element={<RedirectAuth />}></Route>
+      <Route path='/login' element={<HomePage />}></Route>
+      <Route path='/' element={<ProtectedRoutesApp />}>
+        <Route path='app' element={<App />}></Route>
+      </Route>
+    </>
+  )
+);
 
 createRoot(document.getElementById('root')!).render(
   <ChakraProvider>
     <StrictMode>
-      {/* <AuthInitializer>
+      <AuthInitializer>
         <RouterProvider router={router} />
-      </AuthInitializer> */}
-      <App />
+      </AuthInitializer>
     </StrictMode>
   </ChakraProvider>
 )
